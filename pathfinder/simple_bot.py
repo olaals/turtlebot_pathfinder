0e074687e20cf527007e585acf15a8b736470ebb
Michael Hoyer <michael.hoyer3@gmail.com>
2019-10-20 15:31:00 +0200

    implements first publsiher

--- pathfinder/simple_bot.py
+++ pathfinder/simple_bot.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
-from sensor_msgs.msg import LaserScan
 from math import ceil
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
 
 class SimpleBot:
 
@@ -19,9 +20,17 @@ class SimpleBot:
     def main(self):
 
         laser_sub = rospy.Subscriber('/scan', LaserScan, self.update_dist_callback)
+        vel_pub = rospy.Publisher('/mobile_base/commands/velocity', Twist)
 
         while not rospy.is_shutdown():
             print(self.dist_to_wall_in_front)
+            if self.dist_to_wall_in_front > 0.5:
+                msg = Twist()
+                msg.linear.x = 0.3
+                vel_pub.publish(msg)
+            else:
+                msg = Twist()
+                vel_pub.publish(msg)
             self.rate.sleep()
 
 
