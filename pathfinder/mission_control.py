0981d3c2764b26c1624695cee1b7fb3a1e9db871
oyvind1501 <oyvind1401@gmail.com>
2019-10-27 18:38:08 +0100

    skeleton missing only service for vehicle controller

--- pathfinder/mission_control.py
+++ pathfinder/mission_control.py
@@ -3,15 +3,23 @@
 import rospy
 import actionlib
 
-from pathfinder.msg import calculate_pointAction, calculate_pointGoal
+from pathfinder.msg import calculate_pointAction, calculate_pointGoal, go_to_pointAction, go_to_pointGoal
 
 
 class MissionControl:
 
     def __init__(self):
         
+        rospy.loginfo("Setting up client for nav_server...")
         self.nav_client = actionlib.SimpleActionClient("nav_server", calculate_pointAction)
         self.nav_client.wait_for_server()
+        rospy.loginfo("Client for nav_server up and running")
+        
+        rospy.loginfo("Setting up client for vehicle_server...")
+        self.vehicle_client = actionlib.SimpleActionClient("vehicle_server", go_to_pointAction)
+        self.vehicle_client.wait_for_server()
+        rospy.loginfo("Client for vehicle_server up and running")
+
 
     def calc_point(self):
         
@@ -24,6 +32,17 @@ class MissionControl:
     def main(self):
         point = self.calc_point()
         rospy.loginfo(point)
+
+        goal_twist = go_to_pointGoal()
+        goal_twist.goal_twist.linear.x = 1
+
+        self.vehicle_client.send_goal(goal_twist)
+        rospy.loginfo("Waiting for vehicle controller...")
+
+        self.vehicle_client.wait_for_result()
+        res = self.vehicle_client.get_result()
+
+        rospy.loginfo(res)
 
 
 if __name__ == "__main__":
---
+++ pathfinder/vehicle_control.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python 
+
+import rospy
+import actionlib
+
+from pathfinder.msg import go_to_pointAction, go_to_pointResult
+
+
+class VehicleControl:
+
+    _result = go_to_pointResult()
+
+    def __init__(self):
+        self.server = actionlib.SimpleActionServer("vehicle_server", go_to_pointAction, self.go_to_point_cb, False)
+        self.server.start()
+
+    def go_to_point_cb(self, goal):
+        self._result.success = 1
+        self.server.set_succeeded(self._result)
+        # TODO: change success variable to succeeded/aborted
+
+
+if __name__ == "__main__":
+    rospy.init_node("vehicle_control")
+    vehicle_control = VehicleControl()
+    rospy.spin()
