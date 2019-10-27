096f4f5436e0c4ef47ec8e688a3f5df83b66bce1
Michael Hoyer <michael.hoyer3@gmail.com>
2019-10-27 13:25:51 +0100

    creates actions and get_env

---
+++ pathfinder/situational_awareness.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import rospy
+import actionlib
+
+
+def EnvServer:
+    def __init__():
+        pass
+
+    def run():
+        pass
+
+
+if __name__ == "__main__":
+    env_server = EnvServer()
+    env_server.run()
