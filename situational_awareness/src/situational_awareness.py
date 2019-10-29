0b96cdcc9bdcfee4468fb8468a0ab55e0b9b5f53
oyvind1501 <oyvind1401@gmail.com>
2019-10-29 19:54:43 +0100

    move msgs package into git folder

--- situational_awareness/src/situational_awareness.py
+++ situational_awareness/src/situational_awareness.py
@@ -6,14 +6,20 @@ import actionlib
 from tutorial_msgs.msg import get_envFeedback, get_envResult, get_envAction
 
 
-class EnvServer:
+class SituationalAwareness:
+
     # define messages for action server
     _feedback = get_envFeedback()
     _result = get_envResult()
 
-    def __init__(self):    
+    def __init__(self):
+
+        # Create server for nicer data extraction    
         self.env_server = actionlib.SimpleActionServer("env_server", get_envAction, self.get_env_cb, False)
         self.env_server.start()
+
+        # Create client for collision avoidance
+        # TODO: implement collision client
 
     def get_env_cb(self, goal):
         rospy.loginfo("get env callback running")
@@ -27,7 +33,13 @@ class EnvServer:
         self.env_server.set_succeeded(self._result)
 
 
+    def check_for_collision(self):
+        pass
+
+    def main(self):
+        pass
+
 if __name__ == "__main__":
     rospy.init_node("situational_awareness_py")
-    env_server = EnvServer()
+    situational_awareness = SituationalAwareness()
     rospy.spin()
